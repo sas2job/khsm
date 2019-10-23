2a944381b2836ed002190c197ecc3934687f859b
Alex Smirnov <sas2job@gmail.com>
2019-10-23 12:21:14 +0300

    Views for hints and tests for views

---
+++ app/helpers/games_helper.rb
@@ -0,0 +1,11 @@
+module GamesHelper
+  # Этот метод будет рисовать удобный ярлычок для показа статуса игры
+  # Используем стандартные bootstrap-овский класс `label`
+  def game_label(game)
+    if game.status == :in_progress && current_user == game.user
+      link_to content_tag(:span, t("game_statuses.#{game.status}"), class: 'label'), game_path(game)
+    else
+      content_tag :span, t("game_statuses.#{game.status}"), class: 'label label-danger'
+    end
+  end
+end
